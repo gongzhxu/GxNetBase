4f5bc38c17479c76fc8801f7649155f8cc7eb3be
yshe <yshe@4f8759b9-b505-fd4e-b165-fdc0746bd3f7>
2016-08-18 17:11:29 +0800

    添加字符过滤功能
    git-svn-id: https://192.168.5.38/svn/eztalks/ezserver/trunk/project-linux/project-base@7830 4f8759b9-b505-fd4e-b165-fdc0746bd3f7

--- base/BaseUtil.cpp
+++ base/BaseUtil.cpp
@@ -24,24 +24,27 @@ AsyncLogging & getLogger()
 
 void sprintfex(std::string & str, const char * format, ...)
 {
+
     int len = 0;
+    va_list arglist1;
+    va_start(arglist1, format);
 
+    str.resize(256);
+    len = vsnprintf(const_cast<char *>(str.c_str()), str.size(), format, arglist1);
+    if(static_cast<size_t>(len) > str.size())
     {
-        va_list arglist;
-        va_start(arglist, format);
-        len = vsnprintf(0, 0, format, arglist);
-        va_end(arglist);
+        str.resize(len+1);
+
+        va_list arglist2;
+        va_start(arglist2, format);
+        len = vsnprintf(const_cast<char *>(str.c_str()), str.size(), format, arglist2);
+        va_end(arglist2);
     }
 
+    if(len >= 0)
     {
-        va_list arglist;
-        va_start(arglist, format);
-        if(len)
-        {
-            str.resize(len+1);
-            vsnprintf(const_cast<char *>(str.c_str()), str.size(), format, arglist);
-        }
-        va_end(arglist);
+        str.resize(len);
     }
 
+    va_end(arglist1);
 }
